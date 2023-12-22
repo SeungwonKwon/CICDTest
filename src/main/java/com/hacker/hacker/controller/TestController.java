66404b9c0a28d7ba2b250c76a4b68063c7b461a3
SeungwonKwon <seungwon03@khu.ac.kr>
2023-12-22 20:36:41 +0900

    test

--- src/main/java/com/hacker/hacker/controller/TestController.java
+++ src/main/java/com/hacker/hacker/controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("test")
     public String func(){
-        return "hello";
+        return "12345";
     }
 }
